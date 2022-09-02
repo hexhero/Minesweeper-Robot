e50ae12c12a748780f12c654c66f8e326df8eb5a
leo <leo.yang@mspbots.ai>
2022-09-02 19:16:10 +0800

    asd

--- image_check.py
+++ image_check.py
@@ -2,14 +2,16 @@ import pyautogui
 import cv2
 import numpy as np
 
-img = pyautogui.screenshot()
+# img = pyautogui.screenshot()
 
-open_cv_image = np.array(img.crop((302, 687, 463, 848)))
+# open_cv_image = np.array(img.crop((302, 687, 463, 848)))
 
-# Convert RGB to BGR,opencv read image as BGR,but Pillow is RGB
-open_cv_image = cv2.cvtColor(open_cv_image, cv2.COLOR_RGB2BGR)
+# # Convert RGB to BGR,opencv read image as BGR,but Pillow is RGB
+# open_cv_image = cv2.cvtColor(open_cv_image, cv2.COLOR_RGB2BGR)
 
-cv2.imshow('image.jpg', open_cv_image)
+open_cv_image = cv2.imread('image.png')
+
+cv2.imshow('image.jpg', open_cv_image[32:55,0:20])
 
 k = cv2.waitKey(0)
 
